4f207a51a452484b460b1c1ce91755d458be200d
Anita Dam <anitakdam@hotmail.com>
2024-11-07 13:26:26 +0100

    added "test"

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -1,7 +1,10 @@
 package common;
 
+import java.net.ServerSocket;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello klassen!");
+        System.out.println("Hello klassen! ");
+        System.out.println("test");
     }
 }
